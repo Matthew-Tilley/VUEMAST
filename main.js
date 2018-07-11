89f5c1f23380a19ece875879be9c393e07beaee6
Matt Tilley <mtilley255@gmail.com>
2018-07-11 09:59:43 -0500

    Completed second coding challenge per tutorial.

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var app = new Vue({
     el: "#app",
 
     data: {
-        product: "compass",
-        test1: "This ",
-        test2: " is the thing I am selling."
+        product: "socks",
+        image: "greenSocks.jpeg",
+        website: "https://www.msn.com"
         
     }
 })
