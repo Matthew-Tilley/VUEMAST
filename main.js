b4b8745e3be6f35100e963349bd565b12a15bafa
Matt Tilley <mtilley255@gmail.com>
2018-07-11 15:59:53 -0500

    Completed next coding challenge per tutorial.

--- main.js
+++ main.js
@@ -6,7 +6,24 @@ var app = new Vue({
         product: "socks",
         image: "greenSocks.jpeg",
         inventory: 1000,
-        onSale: true
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants:[
+
+            {
+                variantId: 2234,
+                variantColor: "Green"
+            },
+
+            {
+                variantId: 2235,
+                variantColor: "Blue"
+            }
+
+        ],
+
+        sizes: ["Small", "Medium", "Large"]
+
 
         
     }
